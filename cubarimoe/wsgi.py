08532d3837b88127817ecc73df26c91ec6a5ba16
appu1232 <aditya.vad@gmail.com>
2021-03-23 18:10:30 -0400

    rename

---
+++ cubarimoe/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for cubarimoe project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+from pathlib import Path
+
+from django.core.wsgi import get_wsgi_application
+
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
+sys.path.append(str(ROOT_DIR))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cubarimoe.settings.local")
+
+application = get_wsgi_application()
