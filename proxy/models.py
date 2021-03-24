7d7f0e46db8e0cc946828bf829393a38dddf38ab
funkyhippo <funkyhippo@f-ck.me>
2021-03-24 01:07:41 -0700

    Move templates from reader to proxy.

--- proxy/models.py
+++ proxy/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class HitCount(models.Model):
+    proxy_type = models.CharField(max_length=64, blank=False, null=False)
+    proxy_content = models.CharField(max_length=128, blank=False, null=False)
+    hits = models.PositiveIntegerField(("Hits"), default=0)
