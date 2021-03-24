41db853a36792f496983379d1f66dec60ab18824
funkyhippo <funkyhippo@f-ck.me>
2021-03-24 01:54:14 -0700

    Remove more unnecessary routes.

--- homepage/urls.py
+++ homepage/urls.py
@@ -8,16 +8,4 @@ urlpatterns = [
     path("", views.home, name="site-home"),
     path("admin_home/", views.admin_home, name="admin_home"),
     path("about/", views.about, name="site-about"),
-    re_path(
-        r"^(?P<chapter>[\d-]{1,9})/$",
-        views.main_series_chapter,
-        name="site-main-series-chapter",
-    ),
-    re_path(
-        r"^(?P<chapter>[\d-]{1,9})/(?P<page>[\d]{1,9})/$",
-        views.main_series_page,
-        name="site-main-series-page",
-    ),
-    path("latest/", views.latest, name="site-main-series-latest"),
-    path("random/", views.random, name="site-main-series-random"),
 ]
