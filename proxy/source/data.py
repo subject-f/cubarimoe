15b64b219313f29f4b51d51dcfbf3a01a95ec8e0
funkyhippo <funkyhippo@f-ck.me>
2021-04-13 00:09:53 -0700

    API changes for alg.

--- proxy/source/data.py
+++ proxy/source/data.py
@@ -3,15 +3,45 @@ class SeriesAPI:
         self.args = kwargs
 
     def objectify(self):
+        chapters = {}
+
+        for chapter, chapter_data in self.args["chapters"].items():
+            for group_id, group_pages in chapter_data["groups"].items():
+                if chapter not in chapters:
+                    chapters[chapter] = {}
+                chapters[chapter][group_id] = {
+                    "title": chapter_data.get("title", "No title"),
+                    "volume": chapter_data.get("volume", None),
+                }
+                if type(group_pages) is str:
+                    chapters[chapter][group_id]["_images"] = group_pages
+                elif type(group_pages) is list:
+                    base = None
+                    for page in group_pages:
+                        if base is None:
+                            base = page
+                        else:
+                            if len(base) > len(page):
+                                base = base[: len(page)]
+                            for i, (c1, c2) in enumerate(zip(page, base)):
+                                if c1 != c2:
+                                    base = base[:i]
+                                    break
+                    chapters[chapter][group_id]["base"] = base
+                    chapters[chapter][group_id]["images"] = [
+                        p.replace(base, "", 1) for p in group_pages
+                    ]
+
         return {
             "slug": self.args["slug"],
             "title": self.args["title"],
+            "alttitles": self.args.get("alt_titles", []),
             "description": self.args["description"],
             "author": self.args["author"],
             "artist": self.args["artist"],
             "groups": self.args["groups"],
             "cover": self.args["cover"],
-            "chapters": self.args["chapters"],
+            "chapters": chapters,
             "series_name": self.args["title"],
         }
 
