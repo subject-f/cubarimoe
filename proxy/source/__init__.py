1baf9fb390c82353ec1a9ac951e9adf50241262a
funkyhippo <funkyhippo@f-ck.me>
2021-10-08 10:34:12 -0700

    Wrap proxy errors. Consider injecting encrypted traceback for debugging.

--- proxy/source/__init__.py
+++ proxy/source/__init__.py
@@ -41,21 +41,46 @@ class ProxySource(metaclass=abc.ABCMeta):
     def process_description(self, desc):
         return conditional_escape(desc)
 
+    def _api_error(self, request):
+        return render(
+            request,
+            "homepage/thonk_500.html",
+            {"error": "External API error."},
+            status=500,
+        )
+
+    def _processing_error(self, request, exception):
+        return render(
+            request,
+            "homepage/thonk_500.html",
+            {"error": "Processing error. This could be a Cubari problem."},
+            status=500,
+        )
+
     @cache_control(public=True, max_age=60, s_maxage=60)
     def reader_view(self, request, meta_id, chapter, page=None):
         if page:
-            data = self.series_api_handler(meta_id)
+            try:
+                data = self.series_api_handler(meta_id)
+            except Exception as e:
+                return self._processing_error(request, e)
             if data:
                 data = data.objectify()
                 if chapter.replace("-", ".") in data["chapters"]:
                     data["version_query"] = settings.STATIC_VERSION
-                    data["relative_url"] = f"{settings.PROXY_BASE_PATH}/{self.get_reader_prefix()}/{meta_id}"
-                    data["api_path"] = f"/{settings.PROXY_BASE_PATH}/api/{self.get_reader_prefix()}/series/"
+                    data[
+                        "relative_url"
+                    ] = f"{settings.PROXY_BASE_PATH}/{self.get_reader_prefix()}/{meta_id}"
+                    data[
+                        "api_path"
+                    ] = f"/{settings.PROXY_BASE_PATH}/api/{self.get_reader_prefix()}/series/"
                     data["image_proxy_url"] = settings.IMAGE_PROXY_URL
-                    data["reader_modifier"] = f"{settings.PROXY_BASE_PATH}/{self.get_reader_prefix()}"
+                    data[
+                        "reader_modifier"
+                    ] = f"{settings.PROXY_BASE_PATH}/{self.get_reader_prefix()}"
                     data["chapter_number"] = chapter.replace("-", ".")
                     return render(request, "reader/reader.html", data)
-            return render(request, "homepage/thonk_500.html", status=500)
+            return self._api_error(request)
         else:
             return redirect(
                 f"reader-{self.get_reader_prefix()}-chapter-page", meta_id, chapter, "1"
@@ -63,36 +88,49 @@ class ProxySource(metaclass=abc.ABCMeta):
 
     @cache_control(public=True, max_age=60, s_maxage=60)
     def series_view(self, request, meta_id):
-        data = self.series_page_handler(meta_id)
+        try:
+            data = self.series_page_handler(meta_id)
+        except Exception as e:
+            return self._processing_error(request, e)
         if data:
             data = data.objectify()
             data["synopsis"] = self.process_description(data["synopsis"])
             data["version_query"] = settings.STATIC_VERSION
-            data["relative_url"] = f"{settings.PROXY_BASE_PATH}/{self.get_reader_prefix()}/{meta_id}"
-            data["reader_modifier"] = f"{settings.PROXY_BASE_PATH}/{self.get_reader_prefix()}"
+            data[
+                "relative_url"
+            ] = f"{settings.PROXY_BASE_PATH}/{self.get_reader_prefix()}/{meta_id}"
+            data[
+                "reader_modifier"
+            ] = f"{settings.PROXY_BASE_PATH}/{self.get_reader_prefix()}"
             return render(request, "reader/series.html", data)
         else:
-            return render(request, "homepage/thonk_500.html", status=500)
+            return self._api_error(request)
 
     @cache_control(public=True, max_age=60, s_maxage=60)
     def series_api_view(self, request, meta_id):
-        data = self.series_api_handler(meta_id)
+        try:
+            data = self.series_api_handler(meta_id)
+        except Exception as e:
+            return self._processing_error(request, e)
         if data:
             data = data.objectify()
             return HttpResponse(json.dumps(data), content_type="application/json")
         else:
-            return render(request, "homepage/thonk_500.html", status=500)
+            return self._api_error(request)
 
     @cache_control(public=True, max_age=60, s_maxage=60)
     def chapter_api_view(self, request, meta_id):
-        data = self.chapter_api_handler(meta_id)
+        try:
+            data = self.chapter_api_handler(meta_id)
+        except Exception as e:
+            return self._processing_error(request, e)
         if data:
             data = data.objectify()
             return HttpResponse(
                 json.dumps(data["pages"]), content_type="application/json"
             )
         else:
-            return render(request, "homepage/thonk_500.html", status=500)
+            return self._api_error(request)
 
     def register_api_routes(self):
         """Routes will be under /{settings.PROXY_BASE_PATH}/api/<route>"""
