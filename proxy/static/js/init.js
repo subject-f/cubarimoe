ebbb4af822c3c0ee01aa3a0421fdf52172a6a199
funkyhippo <funkyhippo@f-ck.me>
2021-12-22 23:01:48 -0800

    Wrong replacements for b64.

--- proxy/static/js/init.js
+++ proxy/static/js/init.js
@@ -3699,7 +3699,7 @@ function DownloadManager() {
 		let chapURLArray = Reader.SCP.chapterObject.images[Reader.getGroup(chapter)];
 		if (await shouldUseProxy(chapURLArray[0])) {
 			// Base64 URL encode the URLs
-			chapURLArray = chapURLArray.map((url) => `${IMAGE_PROXY_URL}/v1/image/${btoa(url).replace(/\//g, "_").replace(/\//g, "+")}`);
+			chapURLArray = chapURLArray.map((url) => `${IMAGE_PROXY_URL}/v1/image/${btoa(url).replace(/\+/g, "-").replace(/\//g, "_")}`);
 		}
 
 		try {
