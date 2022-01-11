6a46ffedb0b090ac12b41b39876eaa53e44af837
funkyhippo <funkyhippo@f-ck.me>
2022-01-10 23:08:52 -0800

    Tag proxy calls for instrumentation.

--- proxy/source/helpers.py
+++ proxy/source/helpers.py
@@ -30,13 +30,13 @@ def encode(url: str):
 
 
 def get_wrapper(url, *, headers={}, use_proxy=False, **kwargs):
-    url = f"{settings.EXTERNAL_PROXY_URL}/v1/cors/{encode(url)}" if use_proxy else url
+    url = f"{settings.EXTERNAL_PROXY_URL}/v1/cors/{encode(url)}?source=cubari_host" if use_proxy else url
     print(url)
     return requests.get(url, headers={**GLOBAL_HEADERS, **headers}, timeout=8, **kwargs)
 
 
 def post_wrapper(url, headers={}, use_proxy=False, **kwargs):
-    url = f"{settings.EXTERNAL_PROXY_URL}/v1/cors/{encode(url)}" if use_proxy else url
+    url = f"{settings.EXTERNAL_PROXY_URL}/v1/cors/{encode(url)}?source=cubari_host" if use_proxy else url
     return requests.post(url, headers={**GLOBAL_HEADERS, **headers}, timeout=8, **kwargs)
 
 
