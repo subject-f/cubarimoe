6b80af632288c6de5a60b4c831ea06b5a9d24682
Algoinde <algoinde@gmail.com>
2023-12-18 10:58:41 +0500

    Update reddit.py

--- proxy/sources/reddit.py
+++ proxy/sources/reddit.py
@@ -39,13 +39,7 @@ class Reddit(ProxySource):
             {
                 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:122.0) Gecko/20100101 Firefox/122.0",
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-                "Accept-Language": "en-US,en;q=0.5",
-                "Sec-GPC": "1",
-                "Upgrade-Insecure-Requests": "1",
-                "Sec-Fetch-Dest": "document",
-                "Sec-Fetch-Mode": "navigate",
-                "Sec-Fetch-Site": "none",
-                "Sec-Fetch-User": "?1"
+                "Accept-Language": "en-US,en;q=0.5"
             }
         )
         if resp.status_code != 200:
